091fb77b64a83ce5c1996de987dedf7adf100684
stupkalex <Stupkalex@yandex.ru>
2022-03-14 13:55:59 +0300

    initial commit

---
+++ app/src/main/java/com/stupkalex/composition/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.stupkalex.composition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
